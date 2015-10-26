573e71ad56ae06e25f160080983248bf5c3995cc
Dan <refear99@gmail.com>
2015-10-26 17:20:01 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# easy-rongcloud
-This package provides a easy way to access RongCloud Server API. (DEV)
+## easy-rongcloud
+
+This package provides a easy way to access RongCloud API. (DEV)
+ 
+## Require
+
+> PHP >= 5.4.0
+
+## Install
+
+Add this
+
+```
+"refear99/easy-rongcloud": "dev-master"
+```
+
+to your `composer.json`. Then run `composer install` or `composer update`.
+
+## License
+MIT
---
+++ src/Rongcloud.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Rongcloud.php
+ *
+ * Part of Refear99\EasyRongcloud.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    refear99 <refear99@gmail.com>
+ * @copyright 2015 refear99 <refear99@gmail.com>
+ * @link      https://github.com/refear99
+ */
+
+namespace Refear99\EasyRongcloud;
+
+class Sinapay
+{
+}
